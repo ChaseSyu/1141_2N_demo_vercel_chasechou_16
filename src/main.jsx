9da6ff70e1deff6771d3dc4f324b44d816dc0aee
ChaseSyu <ChaseSyu@gmail.com>
2025-10-15 20:47:06 +0800

    install tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import './blog_xx.scss';
 import App_16 from './App_16.jsx';
 
--- src/pages/HomePage_16.jsx
+++ src/pages/HomePage_16.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage_16 = () => {
   return (
-    <div>
-      <h1>HomePage_16</h1>
-      <h2>ChaseChou, 213417016</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-purple-700'>HomePage_16</h1>
+      <h2 className='text-xl'>ChaseChou, 213417016</h2>
     </div>
   );
 };
